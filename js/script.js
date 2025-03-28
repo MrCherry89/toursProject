ca3e4083a20758d0fd9fb402b68a5a0c7303c1b7
MrCherry89 <abgarnati@yandex.ru>
2025-03-28 19:13:32 +0400

    ok

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function () {
-  
+    $(".travel-slider").slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        prevArrow: $(".travel-ideas .top .slider-navigation .slick-prev"),
+        nextArrow: $(".travel-ideas .top .slider-navigation .slick-next"),
+        responsive: [
+            {
+              breakpoint: 1025,
+              settings: {
+                slidesToShow: 3,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                variableWidth: true,
+                autoplay: true,
+                speed: 800,
+              }
+            },
+          ]
+      });
 });
